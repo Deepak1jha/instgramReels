3a87e02b8ddd8cc20e5047b4c53e40ba7682ad3b
Deepak jha <ddjha171@gmail.com>
2020-08-18 23:22:42 +0530

    Updates

---
+++ precache-manifest.c68b7be9770770c41c9c6cb4659262d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e20a33b30532866772ebc79e3e502e1",
+    "url": "/instgramReels/index.html"
+  },
+  {
+    "revision": "cb69455bdf441620a857",
+    "url": "/instgramReels/static/css/main.a9deafa9.chunk.css"
+  },
+  {
+    "revision": "029b0c1adfb5c3ced0dc",
+    "url": "/instgramReels/static/js/2.20249d5a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instgramReels/static/js/2.20249d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb69455bdf441620a857",
+    "url": "/instgramReels/static/js/main.cdab66e2.chunk.js"
+  },
+  {
+    "revision": "b99eef6a68055ed0da0d",
+    "url": "/instgramReels/static/js/runtime-main.3a2224de.js"
+  }
+]);
