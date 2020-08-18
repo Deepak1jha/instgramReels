561f8152ab23031f0c208e1a61c875e19f0e90b9
Deepak jha <ddjha171@gmail.com>
2020-08-18 15:21:29 +0530

    Videos pause and play implemented

--- src/component/videos/VideoCard.js
+++ src/component/videos/VideoCard.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import '../videos/VideoCard.css';
 
 export default function VideoCard() {
 
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (isVideoPlaying) {
+      videoRef.current.pause();
+      setIsVideoPlaying(false)
+    } else {
+      videoRef.current.play();
+      setIsVideoPlaying(true)
+    }
+  }
+
   return (
     <div className="videoCard">
-      <video className="video_player" src={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"} alt="IG Reel Video" loop/>
-      <video className="video_player" src={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"} alt="IG Reel Video" loop/>
-      <video className="video_player" src={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"} alt="IG Reel Video" loop/>
-      <video className="video_player" src={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"} alt="IG Reel Video" loop/>
-      <video className="video_player" src={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"} alt="IG Reel Video" loop/>
-      <video className="video_player" src={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"} alt="IG Reel Video" loop/>
+      <video className="video_player"
+             ref={videoRef}
+             onClick={onVideoPress}
+             src={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"} alt="IG Reel Video"
+             loop/>
     </div>
   )
 }
